3675672a5dbc882d10789c79247b5f523c2fda24
LukasPio <lucaspio.galvao@gmail.com>
2025-06-27 15:07:51 -0300

    feat(linkController): create logic to handle short link requests

--- src/main/java/com/lucas/plinks/LinkController.java
+++ src/main/java/com/lucas/plinks/LinkController.java
@@ -1,4 +1,35 @@
 package com.lucas.plinks;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/")
 public class LinkController {
+
+    private final LinkService linkService;
+
+    public LinkController(LinkService linkService) {
+        this.linkService = linkService;
+    }
+
+    @PostMapping("/short")
+    public ResponseEntity<HashMap<String, String>> shortLink(@RequestBody LinkRequestDTO linkDTO) {
+        long start = System.currentTimeMillis();
+        String shortenedLink = linkService.shortLink(linkDTO);
+
+        String elapsedTime = System.currentTimeMillis() - start + " ms";
+
+        HashMap<String, String> response = new HashMap<>();
+        response.put("status", "200");
+        response.put("shortenedLink", shortenedLink);
+        response.put("elapsedTime", elapsedTime);
+
+        return ResponseEntity.ok(response);
+    }
 }
