6c6e5efa6cf3e54d158e62165ca869d65e8b4b12
LukasPio <lucaspio.galvao@gmail.com>
2025-06-27 15:08:23 -0300

    feat(linkController): create repository to manage links table

--- src/main/java/com/lucas/plinks/LinkRepository.java
+++ src/main/java/com/lucas/plinks/LinkRepository.java
@@ -1,4 +1,7 @@
 package com.lucas.plinks;
 
-public interface LinkRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LinkRepository extends JpaRepository<LinkModel, Long> {
+    boolean existsBySlug(String slug);
 }
