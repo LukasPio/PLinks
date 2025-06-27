21b673f326da0087e354825dfca7407b57189da5
LukasPio <lucaspio.galvao@gmail.com>
2025-06-27 15:07:05 -0300

    feat(plinksProperties): create an auxiliary class to manage constants defined at application.yml

---
+++ src/main/java/com/lucas/plinks/PlinksProperties.java
@@ -0,0 +1,14 @@
+package com.lucas.plinks;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "plink")
+@Getter
+@Setter
+public class PlinksProperties {
+    private String domain;
+}
