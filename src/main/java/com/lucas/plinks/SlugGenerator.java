bcd383c93dc661b8da7ad434e7dbeb1ea39b8df6
LukasPio <lucaspio.galvao@gmail.com>
2025-06-27 15:06:35 -0300

    feat(slugGenerator): create an auxiliary class to generate unique random slugs

---
+++ src/main/java/com/lucas/plinks/SlugGenerator.java
@@ -0,0 +1,18 @@
+package com.lucas.plinks;
+
+import java.security.SecureRandom;
+
+public class SlugGenerator {
+    private static final String BASE62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int SLUG_LENGTH = 6;
+    private static final SecureRandom random = new SecureRandom();
+
+    public static String generateRandomSlug() {
+        StringBuilder stringBuilder = new StringBuilder(SLUG_LENGTH);
+        for (int i = 0; i < SLUG_LENGTH; i++) {
+            int index = random.nextInt(BASE62.length());
+            stringBuilder.append(BASE62.charAt(index));
+        }
+        return  stringBuilder.toString();
+    }
+}
