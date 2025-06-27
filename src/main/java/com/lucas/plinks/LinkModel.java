e1babe3f544f745aa537634a327c10d6c6a79f6a
LukasPio <lucaspio.galvao@gmail.com>
2025-06-27 14:31:14 -0300

    initial commit

---
+++ src/main/java/com/lucas/plinks/LinkModel.java
@@ -0,0 +1,27 @@
+package com.lucas.plinks;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "links")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LinkModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String slug;
+    @Column(name = "original_url")
+    private String originalUrl;
+    private int clicks;
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+}
---
+++ src/main/java/com/lucas/plinks/LinkRepository.java
@@ -0,0 +1,4 @@
+package com.lucas.plinks;
+
+public interface LinkRepository {
+}
