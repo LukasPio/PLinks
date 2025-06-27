c9150c62ef10a46121905e5575030491e9ec7907
LukasPio <lucaspio.galvao@gmail.com>
2025-06-27 15:53:49 -0300

    feat(api): add a new method to findBySlug in link repository

--- src/main/java/com/lucas/plinks/repository/LinkRepository.java
+++ src/main/java/com/lucas/plinks/repository/LinkRepository.java
@@ -1,7 +1,11 @@
-package com.lucas.plinks;
+package com.lucas.plinks.repository;
 
+import com.lucas.plinks.domain.LinkModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface LinkRepository extends JpaRepository<LinkModel, Long> {
     boolean existsBySlug(String slug);
+    Optional<LinkModel> findBySlug(String slug);
 }
