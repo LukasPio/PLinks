820f254d799e646995effc68ba8cf5e823672ddb
LukasPio <lucaspio.galvao@gmail.com>
2025-07-22 19:22:31 -0300

    create model class for links

--- src/main/java/com/lucas/plinks/Link.java
+++ src/main/java/com/lucas/plinks/Link.java
@@ -1,8 +1,18 @@
 package com.lucas.plinks;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "links")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Link {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     String url;
     String slug;
-    int clickCount;
+    int clicks;
 }
