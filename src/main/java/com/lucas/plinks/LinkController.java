8da68c6972db0a3a412762348ddbdbe53b1895ed
LukasPio <lucaspio.galvao@gmail.com>
2025-07-22 19:19:47 -0300

    create route to redirect from shortened link

--- src/main/java/com/lucas/plinks/LinkController.java
+++ src/main/java/com/lucas/plinks/LinkController.java
@@ -4,6 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Timestamp;
+
 @RestController
 @RequestMapping("/")
 public class LinkController {
@@ -14,10 +18,14 @@ public class LinkController {
         this.linkService = linkService;
     }
 
-    @PostMapping("/short/{url}")
-    public ResponseEntity<String> shortUrl(@PathVariable String url) {
-        String shortenedLink = linkService.shortUrl(url);
-        return ResponseEntity.status(HttpStatus.OK).body(shortenedLink);
+    @PostMapping("/short")
+    public ResponseEntity<ApiResponse<String>> shortUrl(@RequestBody LinkRequestDTO requestDTO) {
+        String shortenedLink = linkService.shortUrl(requestDTO);
+        ApiResponse<String> response = new ApiResponse<>("Link shortened successfully", new Timestamp(
+                System.currentTimeMillis()),
+                HttpStatus.OK.value(),
+                shortenedLink);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @GetMapping("/{slug}")
@@ -25,5 +33,13 @@ public class LinkController {
         String url = linkService.getUrlFromSlug(slug);
         if (url == null) return "Not found";
         return "redirect:" + url;
+    public ResponseEntity<Void> redirect(@PathVariable String slug) throws URISyntaxException {
+        String url = linkService.redirect(slug);
+        if (url == null) {
+            URI notFound = new URI(Constants.BASE_URL + "/not-found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).location(notFound).build();
+        }
+        return ResponseEntity.status(HttpStatus.FOUND).location(new URI(url)).build();
+    }
     }
 }
