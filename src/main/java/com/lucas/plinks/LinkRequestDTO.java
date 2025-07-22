9daa6825f605258ee54a6f4131966561ee6ea8c5
LukasPio <lucaspio.galvao@gmail.com>
2025-07-22 19:19:13 -0300

    create DTOs to receive data

---
+++ src/main/java/com/lucas/plinks/LinkRequestDTO.java
@@ -0,0 +1,6 @@
+package com.lucas.plinks;
+
+import jakarta.annotation.Nullable;
+
+public record LinkRequestDTO(String url, @Nullable String slug) {
+}
---
+++ src/main/java/com/lucas/plinks/SlugRequestDTO.java
@@ -0,0 +1,4 @@
+package com.lucas.plinks;
+
+public record SlugRequestDTO(String slug) {
+}
